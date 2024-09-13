205adf23d1609f59cdbd7faed34108d76fa7621d
leonardoWer <102314754+leonardoWer@users.noreply.github.com>
2024-09-13 15:23:06 +0300

    lab0
    Доработал код, учёл необходимые ограничения

--- lab0/lab0.py
+++ lab0/lab0.py
@@ -1,45 +1,62 @@
 import time
 
 # ex1.1
-a,b = map(int, input("Введите число a и через пробел число b: ").split())
-print(a + b)
+# a,b = map(int, input("Введите число a и через пробел число b: ").split())
+# if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+#     print(a + b)
+# else:
+#     print("Числа должны удовлетворять условию |a|<=10**9, |b|<=10**9")
 
 # ex1.2
-a,b = map(int, input("Введите число a и через пробел число b: ").split())
-print(a + b**2)
+# a,b = map(int, input("Введите число a и через пробел число b: ").split())
+# if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+#     print(a + b**2)
+# else:
+#     print("Числа должны удовлетворять условию |a|<=109, |b|<=109")
+
 
 # ex1.3
-input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
-output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
+# input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
+# output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
 
-a, b = map(int, input.readline().split())
-output.write(str(a+b))
-output.close()
+# a, b = map(int, input.readline().split())
+# if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+#     output.write(str(a+b))
+#     output.close()
+# else:
+#     print("Числа должны удовлетворять условию |a|<=109, |b|<=109")
+
 
 # ex1.4
-input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
-output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
+# input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
+# output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
 
-a, b = map(int, input.readline().split())
-output.write(str(a+b**2))
-output.close()
+# a, b = map(int, input.readline().split())
+# if (-10**9<=a<=10**9) and (-10**9<=b<=10**9):
+#     output.write(str(a+b**2))
+#     output.close()
+# else:
+#     print("Числа должны удовлетворять условию |a|<=109, |b|<=109")
+
 
 # ex2
-input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
-output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
+# input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
+# output = open(r"C:\Python\ITMO\Algosi\lab0\output.txt", "w")
 
-t_start = time.perf_counter()
+# t_start = time.perf_counter()
 
-n = int(input.readline())
+# n = int(input.readline())
+# if (0<=n<=45):
+#     fib = [0,1]
+#     for i in range(2,46):
+#         fib.append(fib[i-2]+fib[i-1])
 
-fib = [0,1]
-for i in range(2,45):
-    fib.append(fib[i-2]+fib[i-1])
+#     output.write(str(fib[n]))
+#     output.close()
+# else:
+#     print("Введённый элемент не должен превышать 45")
 
-output.write(str(fib[n]))
-output.close()
-
-print("Время работы: %s секунд" % (time.perf_counter() - t_start))
+# print("Время работы: %s секунд" % (time.perf_counter() - t_start))
 
 # ex3
 input = open(r"C:\Python\ITMO\Algosi\lab0\input.txt")
@@ -54,11 +71,14 @@ def fib(n):
         return n
     f1, f2 = 0,1
     for i in range(2,n+1):
-        f1, f2 = f2, f1+f2
+        f1, f2 = (f2%10), (f1+f2)%10
     return f2
 
-output.write(str(fib(n)%10))
-output.close()
+if (0<=n<=10**7):
+    output.write(str(fib(n)%10))
+    output.close()
+else:
+    print("Время работы: %s секунд" % (time.perf_counter() - t_start))
 
 print("Время работы: %s секунд" % (time.perf_counter() - t_start))
 
