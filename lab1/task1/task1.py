f69adad5e5f681a9ce127e74da0cf92fbfe6f750
leonardo_Wer <name89213126414@gmail.com>
2024-10-03 19:11:57 +0300

    Решил первое задание

---
+++ lab1/task1/task1.py
@@ -0,0 +1,14 @@
+file_in = open("input.txt")
+file_out = open("output.txt", "w")
+
+n = int(file_in.readline()) #Количество элементов
+lst = [int(el) for el in file_in] #Список с элементами
+for i in range(1, n):
+    key = lst[i]
+    j = i-1
+    while (j>=0) and (lst[j]>key):
+        lst[j+1] = lst[j]
+        j -=1
+    lst[j+1] = key
+lst = [str(el) for el in lst]
+file_out.write(" ".join(lst))
