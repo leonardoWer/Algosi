c9297a3e907111633a6ad3d80a6e89ab1230f568
leonardo_Wer <name89213126414@gmail.com>
2024-10-08 17:00:38 +0300

    Написал тесты для всех заданий

---
+++ lab1/task2/tests/test.py
@@ -0,0 +1,20 @@
+from lab1.task2.src.task2 import insertion_sort
+import datetime
+import tracemalloc
+
+file_in = open("../txtfiles/input.txt")
+
+n = int(file_in.readline()) #Количество элементов
+lst = list(map(int, file_in.readline().split())) #Список с элементами
+
+tracemalloc.start() # Запускаем счётчик памяти
+start_time = datetime.datetime.now() # Запускаем счётчик времени
+
+print(insertion_sort(n, lst)) # Выводим результат отработанной функции
+
+finish_time = datetime.datetime.now() # Измеряем время конца работы
+print("Итоговое время:",finish_time - start_time) # Выводим итоговое время
+
+current, peak = tracemalloc.get_traced_memory() # Присваеваем двум переменным память, используемую сейчас, и на пике
+print(f"Используемая память: {current / 10**6} МБ\nПамять на пике: {peak / 10**6} МБ") #Выводим время работы в мегабайтах
+
