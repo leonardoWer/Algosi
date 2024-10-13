2aadf6d4123d49de053a9e2903b30f6a871325f5
leonardo_Wer <name89213126414@gmail.com>
2024-10-13 17:26:33 +0300

    Добавил докстринги

--- lab1/task6/src/task6.py
+++ lab1/task6/src/task6.py
@@ -9,6 +9,11 @@ n = int(file_in.readline()) #Количество элементов
 lst = list(map(int, file_in.readline().split())) #Список с элементами
 
 def bubble_sort(n:int, lst:list) ->str:
+    """
+    - Проходимся по всем парам элементов массива
+    - Если элементы в невозрастающем порядке - меняем их местами
+    - Переходим к следующей паре
+    """
     for i in range(n-1):
         for j in range(i+1, n):
             if lst[j] <= lst[i]:
