181b0452a90f0156d6e16b30a4b91a78b00365c8
leonardo_Wer <name89213126414@gmail.com>
2024-10-28 19:11:25 +0300

    Добавил в юнит базовый тест функции

--- lab2/utils.py
+++ lab2/utils.py
@@ -3,6 +3,9 @@
  - Чтение файла input.txt
  - Запись в файл output.txt
 """
+import tracemalloc
+import datetime
+
 
 import_path = "../txtfiles/"
 
@@ -55,3 +58,17 @@ def read_file_4() -> (int, list, int, list):
     variable_4 = list(map(int, file_in.readline().split()))
 
     return variable_1, variable_2, variable_3, variable_4
+
+def test_func_on_data_from_example(func, n:int, lst:list):
+    # На данных из примера
+    print("Просчитаем время и память работы сортировки")
+    tracemalloc.start()  # Запускаем счётчик памяти
+    start_time = datetime.datetime.now()  # Запускаем счётчик времени
+
+    print(func(lst))
+
+    finish_time = datetime.datetime.now()  # Измеряем время конца работы
+    print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
+
+    current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
+    print(f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
