b13f13362a2f199d089d232304af6e2bf6baf8dd
leonardo_Wer <name89213126414@gmail.com>
2024-10-28 18:00:50 +0300

    Переделал задание 1 с учётом утилс

--- lab2/task1/src/task1.py
+++ lab2/task1/src/task1.py
@@ -1,12 +1,7 @@
 """
 Сортировка слиянием
 """
-
-file_in = open("../txtfiles/input.txt")
-file_out = open("../txtfiles/output.txt", "w")
-
-n = int(file_in.readline())  # Количество элементов
-lst = list(map(int, file_in.readline().split()))  # Список с элементами
+from lab2 import utils
 
 
 def merge_sort(lst: list) -> list:
@@ -35,7 +30,7 @@ def merge(lst1: list, lst2: list) -> list:
     res = []
     lst1_len = len(lst1)
     lst2_len = len(lst2)
-    i, j = 0, 0  # Начинаем сранивать элементы с 0
+    i, j = 0, 0  # Начинаем сравнивать элементы с 0
     while i < lst1_len and j < lst2_len:  # Пока не дойдём до конца списков
         if lst1[i] <= lst2[j]:
             res.append(lst1[i])
@@ -46,5 +41,8 @@ def merge(lst1: list, lst2: list) -> list:
     res += lst1[i:] + lst2[j:]  # Добавляем оставшиеся элементы
     return res
 
-res = " ".join([str(el) for el in merge_sort(lst)]) # Список с результатом приводим к строке и записываем в файл
-file_out.write(res)
+
+if __name__ == "__main__":
+    n, lst = utils.read_file()  # Количество элементов и список с элементами
+    res = merge_sort(lst)
+    utils.write_file([res])
