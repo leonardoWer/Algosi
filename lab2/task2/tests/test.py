6834f99c9bb7a320cba767ee2e5676f4211d9560
leonardo_Wer <name89213126414@gmail.com>
2024-10-28 19:13:15 +0300

    Переделал тест 2 под функцию

--- lab2/task2/tests/test.py
+++ lab2/task2/tests/test.py
@@ -3,19 +3,24 @@ from lab2 import utils
 import datetime
 import tracemalloc
 
-
 n, lst = utils.read_file()
 
 
-# На данных из примера
-print("Просчитаем время и память работы сортировки")
-tracemalloc.start()  # Запускаем счётчик памяти
-start_time = datetime.datetime.now()  # Запускаем счётчик времени
+def test_baza(func, lst, n):
+    # На данных из примера
+    print("Просчитаем время и память работы сортировки")
+    tracemalloc.start()  # Запускаем счётчик памяти
+    start_time = datetime.datetime.now()  # Запускаем счётчик времени
 
-print(merge_sort(lst,0, n-1))
+    print(merge_sort(lst, 0, n - 1))
 
-finish_time = datetime.datetime.now()  # Измеряем время конца работы
-print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
+    finish_time = datetime.datetime.now()  # Измеряем время конца работы
+    print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
 
-current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
-print(f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
+    current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
+    print(
+        f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
+
+
+if __name__ == "__main__":
+    test_baza(merge_sort, lst, n)
