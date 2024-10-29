51e21e0cefd3d31383cfadf5893300f1e1c158dd
leonardo_Wer <name89213126414@gmail.com>
2024-10-29 16:07:57 +0300

    Сделал тест к заданию 5 по рекомендациям используя юнит тест и принцип given when then

--- lab2/task5/tests/test.py
+++ lab2/task5/tests/test.py
@@ -2,16 +2,33 @@ from lab2.task5.src.task5 import find_el_bolsh
 from lab2 import utils
 import datetime
 import tracemalloc
+import unittest
 
-n, lst = utils.read_file()
 
-tracemalloc.start()  # Запускаем счётчик памяти
-start_time = datetime.datetime.now()  # Запускаем счётчик времени
+class TaskTest(unittest.TestCase):
 
-print(find_el_bolsh(lst, n))  # Выводим результат отработанной функции
+    def test_sort(self):
+        """Тест на данных из примера"""
+        # given
+        n, lst = utils.read_file()
 
-finish_time = datetime.datetime.now()  # Измеряем время конца работы
-print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
+        # when
+        print("Просчитаем время и память работы алгоритма")
+        tracemalloc.start()  # Запускаем счётчик памяти
+        start_time = datetime.datetime.now()  # Запускаем счётчик времени
 
-current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
-print(f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ")  # Выводим время работы в мегабайтах
+        print(find_el_bolsh(lst, n))
+
+        finish_time = datetime.datetime.now()  # Измеряем время конца работы
+        print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
+
+        current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
+        print(
+            f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
+
+        # then
+        self.assertEqual(find_el_bolsh(lst, n), "1")
+
+
+if __name__ == "__main__":
+    unittest.main()
