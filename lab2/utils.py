42765a78c1e9e6e313561b5bfb314d6d39a8e0c7
leonardo_Wer <name89213126414@gmail.com>
2024-10-29 15:38:56 +0300

    Добаввил закрытие файла в утилс чтобы не было ошибки при тестах Убрал лишнее

--- lab2/utils.py
+++ lab2/utils.py
@@ -5,7 +5,7 @@
 """
 import tracemalloc
 import datetime
-
+from os import close
 
 import_path = "../txtfiles/"
 
@@ -20,6 +20,7 @@ def read_file() -> (int, list):
     file_in = open(path)
     variable_1 = int(file_in.readline())
     variable_2 = list(map(int, file_in.readline().split()))
+    file_in.close()
 
     return variable_1, variable_2
 
@@ -42,6 +43,7 @@ def write_file(data: list):
             file_out.write(el)
         if len(data) > 1 and data[-1] != el:
             file_out.write("\n")
+    file_out.close()
 
 
 def read_file_4() -> (int, list, int, list):
@@ -56,19 +58,6 @@ def read_file_4() -> (int, list, int, list):
     variable_2 = list(map(int, file_in.readline().split()))
     variable_3 = int(file_in.readline())
     variable_4 = list(map(int, file_in.readline().split()))
+    file_in.close()
 
     return variable_1, variable_2, variable_3, variable_4
-
-def test_func_on_data_from_example(func, n:int, lst:list):
-    # На данных из примера
-    print("Просчитаем время и память работы сортировки")
-    tracemalloc.start()  # Запускаем счётчик памяти
-    start_time = datetime.datetime.now()  # Запускаем счётчик времени
-
-    print(func(lst))
-
-    finish_time = datetime.datetime.now()  # Измеряем время конца работы
-    print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
-
-    current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
-    print(f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
