f1e0678a7f045432ff1c92882ffad8c39c8ed3fc
leonardo_Wer <name89213126414@gmail.com>
2024-10-30 11:44:12 +0300

    Сделал рефакторинг теста и самого задания номер 1

--- lab1/task1/tests/test.py
+++ lab1/task1/tests/test.py
@@ -1,20 +1,34 @@
 from lab1.task1.src.task1 import insertion_sort
 import datetime
 import tracemalloc
+from lab1 import utils
+import unittest
 
-file_in = open("../txtfiles/input.txt")
 
-n = int(file_in.readline()) #Количество элементов
-lst = list(map(int, file_in.readline().split())) #Список с элементами
+class TaskTest(unittest.TestCase):
 
-tracemalloc.start() # Запускаем счётчик памяти
-start_time = datetime.datetime.now() # Запускаем счётчик времени
+    def test_sort(self):
+        """Тест на данных из примера"""
+        # given
+        n, lst = utils.read_file()
 
-print(insertion_sort(n, lst)) # Выводим результат отработанной функции
+        # when
+        print("Просчитаем время и память работы алгоритма")
+        tracemalloc.start()  # Запускаем счётчик памяти
+        start_time = datetime.datetime.now()  # Запускаем счётчик времени
 
-finish_time = datetime.datetime.now() # Измеряем время конца работы
-print("Итоговое время:",finish_time - start_time) # Выводим итоговое время
+        print(insertion_sort(n, lst))
 
-current, peak = tracemalloc.get_traced_memory() # Присваеваем двум переменным память, используемую сейчас, и на пике
-print(f"Используемая память: {current / 10**6} МБ\nПамять на пике: {peak / 10**6} МБ") #Выводим время работы в мегабайтах
+        finish_time = datetime.datetime.now()  # Измеряем время конца работы
+        print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
 
+        current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
+        print(
+            f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
+
+        # then
+        self.assertEqual(insertion_sort(n, lst), [26, 31, 41, 41, 58, 59])
+
+
+if __name__ == "__main__":
+    unittest.main()
