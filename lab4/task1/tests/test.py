ed9ca07e811e5f01bd37581b74deb3ab5325da2b
leonardo_Wer <name89213126414@gmail.com>
2024-11-25 17:36:11 +0300

    Добавил проверку на время и память в тест к 1-ому заданию

--- lab4/task1/tests/test.py
+++ lab4/task1/tests/test.py
@@ -1,6 +1,8 @@
 from lab4 import utils
 import unittest
 from lab4.task1.src.task1 import Stack, Utils
+import tracemalloc
+import datetime
 
 
 class TaskTest1(unittest.TestCase):
@@ -12,6 +14,7 @@ class TaskTest1(unittest.TestCase):
         # На примере
         my_stack = Stack() # Создаём стек
         stack_utils = Utils() # Создаём утилс для считывания из файла
+        stack_utils.read_stack_data()  # Считываем данные из файла
 
         # Остальные случаи
         stack1 = Stack()
@@ -20,9 +23,19 @@ class TaskTest1(unittest.TestCase):
         # when
 
         # На примере
-        stack_utils.read_stack_data()  # Считываем данные из файла
+        print(f"Просчитаем время и память работы stack")
+        tracemalloc.start()  # Запускаем счётчик памяти
+        start_time = datetime.datetime.now()  # Запускаем счётчик времени
+
         stack_utils.fill_commands_list()  # Переделываем операции в команды
         my_stack, result = stack_utils.fill_stack(my_stack)  # Заполняем стек и получаем список с удалёнными элементами
+
+        finish_time = datetime.datetime.now()  # Измеряем время конца работы
+        print("Итоговое время:", finish_time - start_time)  # Выводим итоговое время
+
+        current, peak = tracemalloc.get_traced_memory()  # Присваеваем двум переменным память, используемую сейчас, и на пике
+        print(
+            f"Используемая память: {current / 10 ** 6} МБ\nПамять на пике: {peak / 10 ** 6} МБ\n")  # Выводим время работы в мегабайтах
 
         # Остальные случаи
         stack1.push(4)
